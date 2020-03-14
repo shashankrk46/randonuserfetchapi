4c12587b3df5186851c49e6e6592f51c73c34954
shashankrk46 <shashankrk46@gmail.com>
2020-03-14 16:49:04 +0530

    fixed UI error

--- ui.js
+++ ui.js
@@ -4,17 +4,17 @@ class UI{
     }
 
     sketch(user){
-        console.log(user);
+        console.log(user[0].name.first);
         this.profile.innerHTML=`
         <ul class="list-group">
-        <li class="list-group-item">Name:${user.name} </li>
-        <li class="list-group-item">Age:${user.Age}</li>
-        <li class="list-group-item">Location:${user.Location}</li>
-        <li class="list-group-item">Preference:${user.gender} looking for ${user.lookingfor}</li>
+        <li class="list-group-item">Firstname:${user[0].name.first} <br> Lastname:${user[0].name.last} </li>
+        <li class="list-group-item">Age:${user[0].dob.age}</li>
+        <li class="list-group-item">Location:${user[0].location.country}</li>
+        <li class="list-group-item">Gender:${user[0].gender} </li>
         </ul>`;
 
         document.getElementById('imageDisplay').innerHTML=`
-        <img src=${user.image}">;
-`
+        <img src=${user[0].picture.large}>`;
+
     }
 }
